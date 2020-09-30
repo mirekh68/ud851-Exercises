05476c62ecc077d867ee3f1436d7941398918767
Mirek <miroslaw.habarta@gmail.com>
2020-09-30 15:18:10 +0200

    Solved 2nd exercise - todo fixes

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
     }
-    // TODO (11) Override onOptionsItemSelected
+    // xTODO (11) Override onOptionsItemSelected
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 }
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    // xTODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
+    // xTODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+    // xTODO (14) Don't forgot to call .show() on your Toast
+    // xTODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
 }
